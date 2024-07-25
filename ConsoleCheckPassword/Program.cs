c36ba39bac789e4886675df8739b74c3a5b18614
wernerdc <manuel.werner3@gmx.de>
2024-07-25 08:44:40 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleCheckPassword/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleCheckPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool appRunning = true;
+
+            while (appRunning)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Clear();
+                Console.WriteLine("ConsoleCheckPassword \n");
+
+
+
+
+
+
+
+                Console.Write("\n\nProgramm beenden (e)? ");
+                try {
+                    string exitApp = Console.ReadLine();
+                    if (exitApp.ToUpper() == "E") {
+                        appRunning = false;
+                    }
+                } catch {
+                    // no error message, just keep going and repeat the app 
+                }
+            }
+        }
+    }
+}
